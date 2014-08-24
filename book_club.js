5a08743477b5fb224ce5615f5e89d14d5d2647d1
gscoular <gscoular@gmail.com>
2014-08-24 14:45:05 -0600

    Included header links

---
+++ book_club.js
@@ -0,0 +1,7 @@
+
+var books_link = 'book_club.html';
+var people_link = 'people.html';
+var potluck_link = 'pot_luck.html';
+document.getElementById('books_link').setAttribute('href',books_link);
+document.getElementById('people_link').setAttribute('href',people_link);
+document.getElementById('potluck_link').setAttribute('href',potluck_link);
